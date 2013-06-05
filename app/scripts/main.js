53378765e309748ef75df1f098e21503ed3df4c9
Daniel Beeke <daniel.beeke@gmail.com>
2013-06-06 00:39:20 +0200

    init

---
+++ app/scripts/main.js
@@ -0,0 +1,26 @@
+$(function() {
+
+  var map = L.map('map', {
+    attributionControl: false
+  }).setView([51.505, -0.09], 13);
+
+  L.tileLayer('http://{s}.tiles.mapbox.com/v3/examples.map-vyofok3q/{z}/{x}/{y}.png').addTo(map);
+
+  var features = new L.geoJson;
+
+  var url = "https://docs.google.com/spreadsheet/pub?key=0Au2cOawuTPZWdDM2YWwzWU40a0tKbWlIVW00S3pyaFE&output=html";
+  var googleSpreadsheet = new GoogleSpreadsheet();
+  googleSpreadsheet.url(url);
+  googleSpreadsheet.load(function(result) {
+    $.each(result.items, function(index, company) {
+      if (company.lat && company.long) {
+        L.marker([company.lat, company.long]).addTo(features);
+      }
+    });
+
+    features.addTo(map);
+    var bounds = features.getBounds();
+    map.fitBounds(bounds);
+  });
+
+});
